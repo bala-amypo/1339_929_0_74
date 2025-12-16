c9a174c8f3e22d8cab5b332030e9dbe4c8048ae5
bala-amypo <bala@amypo.in>
2025-12-16 15:14:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/RouteOptimizationResult.java
+++ src/main/java/com/example/demo/entity/RouteOptimizationResult.java
@@ -1,10 +1,61 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class RouteOptimizationResult {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private Shipment shipment;
+    private Double optimizedDistanceKm;
+    private Double estimatedFuelUsageL;
+    private LocalDateTime generatedAt;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Shipment getShipment() {
+		return shipment;
+	}
+	public void setShipment(Shipment shipment) {
+		this.shipment = shipment;
+	}
+	public Double getOptimizedDistanceKm() {
+		return optimizedDistanceKm;
+	}
+	public void setOptimizedDistanceKm(Double optimizedDistanceKm) {
+		this.optimizedDistanceKm = optimizedDistanceKm;
+	}
+	public Double getEstimatedFuelUsageL() {
+		return estimatedFuelUsageL;
+	}
+	public void setEstimatedFuelUsageL(Double estimatedFuelUsageL) {
+		this.estimatedFuelUsageL = estimatedFuelUsageL;
+	}
+	public LocalDateTime getGeneratedAt() {
+		return generatedAt;
+	}
+	public void setGeneratedAt(LocalDateTime generatedAt) {
+		this.generatedAt = generatedAt;
+	}
+	public RouteOptimizationResult(Long id, Shipment shipment, Double optimizedDistanceKm, Double estimatedFuelUsageL,
+			LocalDateTime generatedAt) {
+		this.id = id;
+		this.shipment = shipment;
+		this.optimizedDistanceKm = optimizedDistanceKm;
+		this.estimatedFuelUsageL = estimatedFuelUsageL;
+		this.generatedAt = generatedAt;
+	}
+	public RouteOptimizationResult() {
+	}
     
+
 }
