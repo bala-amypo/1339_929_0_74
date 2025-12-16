a27b4d0810d68443039456e61d28b4fac96c36ab
bala-amypo <bala@amypo.in>
2025-12-16 17:16:51 +0000

    User commit

--- src/main/java/com/example/demo/entity/RouteOptimizationResult.java
+++ src/main/java/com/example/demo/entity/RouteOptimizationResult.java
@@ -6,8 +6,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 
 @Entity
+@Builder
 public class RouteOptimizationResult {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
