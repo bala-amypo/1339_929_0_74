407c2803e6f147c90a61ae420a87f57d2a0f940b
bala-amypo <bala@amypo.in>
2025-12-16 17:07:11 +0000

    User commit

--- src/main/java/com/example/demo/controller/ShipmentController.java
+++ src/main/java/com/example/demo/controller/ShipmentController.java
@@ -1,9 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/shipments")
 public class ShipmentController {
-    @PostMapping("/{}")
+    @PostMapping("/{vehicleId}")
+    public void done(){
+
+    }
+    @GetMapping("/{shipmentId}")
+    public void ans(){
+
+    }
 }
--- src/main/java/com/example/demo/controller/VehicleController.java
+++ src/main/java/com/example/demo/controller/VehicleController.java
@@ -1,8 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/vehicles")
 public class VehicleController {
-    
+    @PostMapping("/{userId}")
+    public void ans(){
+
+    }
+    @GetMapping("/user/{userId}")
+    public void done(){
+
+    }
 }
