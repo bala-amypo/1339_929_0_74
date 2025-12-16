faef84090d6a21e85cf32f671507664a10695832
bala-amypo <bala@amypo.in>
2025-12-16 17:04:04 +0000

    User commit

--- src/main/java/com/example/demo/controller/RouteOptimizationController.java
+++ src/main/java/com/example/demo/controller/RouteOptimizationController.java
@@ -1,9 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/optimize")
 public class RouteOptimizationController {
-    
+    @PostMapping("/{shipmentId}")
+    public void done(){
+
+    }
+    @GetMapping("/result/{resultId}")
+    public void func(){
+        
+    }
 }
--- src/main/java/com/example/demo/controller/ShipmentController.java
+++ src/main/java/com/example/demo/controller/ShipmentController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/shipments")
 public class ShipmentController {
-    
+    @PostMapping("/{}")
 }
