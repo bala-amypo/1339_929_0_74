4cc54b2b957c6fcd8fbd9fab7cddf34718f6f14b
bala-amypo <bala@amypo.in>
2025-12-16 16:59:03 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,8 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
-    
+    @PostMapping("/login")
+    public void done(){
+
+    }
+    @PostMapping("/register")
+    public void reg(){
+        
+    }
 }
--- src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
@@ -1,7 +1,30 @@
 package com.example.demo.service.impl;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Vehicle;
 import com.example.demo.service.VehicleService;
 @Service
 public class VehicleServiceImpl implements VehicleService {
+
+	@Override
+	public Vehicle addVehicle(Long userId, Vehicle vehicle) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'addVehicle'");
+	}
+
+	@Override
+	public List<Vehicle> getVehiclesByUser(Long userId) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getVehiclesByUser'");
+	}
+
+	@Override
+	public Vehicle findById(Long id) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'findById'");
+	}
     
 }
