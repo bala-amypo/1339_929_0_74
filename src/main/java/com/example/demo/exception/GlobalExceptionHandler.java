f83d3618f3bca2fb961b56724fe04135ccbcd300
bala-amypo <bala@amypo.in>
2025-12-19 04:38:09 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -7,5 +7,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    
+    public String hello(){
+        return "hello";
+    }
+
+    @ExceptionHandler(PasswordNotMatchCri.class)
+    public String hi(){
+        return "Hi";
+    }
 }
